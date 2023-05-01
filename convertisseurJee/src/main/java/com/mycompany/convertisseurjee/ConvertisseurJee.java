9ac02d86a779a530e5b9f9956dd4c0f435962c1f
marcelle690 <mnouteza@gmail.com>
2023-05-01 18:42:39 +0100

    mon convertisseur monetaire
    jee licence3

---
+++ convertisseurJee/src/main/java/com/mycompany/convertisseurjee/ConvertisseurJee.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.convertisseurjee;
+
+/**
+ *
+ * @author Marcy Winner's
+ */
+public class ConvertisseurJee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
